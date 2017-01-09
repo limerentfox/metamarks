c5968ab0bb63a05016976871b50b25b9799d57fc
wongjenn <wongjenn@yahoo.com>
2017-01-09 16:39:08 -0500

    create login process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  private
+  def current_user
+    if logged_in?
+      session[:user_id]
+    end
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+end
