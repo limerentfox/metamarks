709a71a2e0e27b6b7c3cf1d7ce603b044efde4a0
kevwang424 <kev.wang424@gmail.com>
2017-01-09 16:43:38 -0500

    Add bookmarks and tags

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,43 @@
+class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.update(bookmark_params)
+      redirect_to bookmark_path(@bookmark)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:url, :user_id, :all_tags)
+  end
+end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,17 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   has_many :bookmark_tags
   has_many :tags, through: :bookmark_tags
+  validates :url, presence: true
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
+
+
 end
