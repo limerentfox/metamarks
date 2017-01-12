753bb28097536ec7003abc06c28b5124653e3e3a
kevwang424 <kev.wang424@gmail.com>
2017-01-12 13:32:29 -0500

    added username and circlular image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112145711) do
+ActiveRecord::Schema.define(version: 20170112181848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170112145711) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "name"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,19 @@ require_relative '../rails_helper'
 describe User do
   context 'validations' do
 
-    let(:user) {User.new(email: 'christi@gmail.com', password: '1234')}
-    let(:invalid_user_email) {User.new(password: '1234')}
-    let(:invalid_user_password) {User.new(email: 'christi@gmail.com')}
+    let(:user) {User.new(name: "Christi", email: 'christi@gmail.com', password: '1234')}
+    let(:invalid_user_email) {User.new(name: "Christi", password: '1234')}
+    let(:invalid_user_password) {User.new(name: "Christi", email: 'christi@gmail.com')}
+    let(:invalid_user_name) {User.new(email: 'christi@gmail.com', email: 'christi@gmail.com')}
     let(:invalid_user_bad_format) {User.new(email: 'christi@gmail')}
     let(:same_user) {User.new(email: 'christi@gmail.com', password: '12345')}
 
-    it 'is valid with a email and a password' do
+    it 'is valid with a name, a email and a password' do
       expect(user).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      expect(invalid_user_name).to_not be_valid
     end
 
     it 'is invalid without a email' do
