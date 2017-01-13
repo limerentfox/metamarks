3b993580ede1eda06b47c376e9f157ade5a37bcf
curiositypaths <curiositypaths@users.noreply.github.com>
2017-01-13 13:01:50 -0500

    Refactor popular tags

--- app/models/bookmark_tag.rb
+++ app/models/bookmark_tag.rb
@@ -10,14 +10,7 @@ class BookmarkTag < ApplicationRecord
   private
 
   def self.get_popular_tags_id
-    all.each_with_object(popular_tags ||= {}) do |bt|
-      if popular_tags[bt.tag_id.to_s]
-        popular_tags[bt.tag_id.to_s] += 1
-      else
-        popular_tags[bt.tag_id.to_s] = 1
-      end
-    end
-    popular_tags.sort_by {|k, v| v}.reverse[0..2]
+    all.group(:tag_id).count(:id).sort_by {|k,v|v}.reverse[0..2]
   end
 
   def self.get_popular_tags_names(ids)
