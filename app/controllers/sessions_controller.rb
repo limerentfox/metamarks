e3d123e28a806f46e8f545ab4862fdf092092634
curiositypaths <curiositypaths@users.noreply.github.com>
2017-01-13 11:48:46 -0500

    Redict to index after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to bookmarks_path
     else
       render :new
     end
